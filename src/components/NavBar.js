31a6563182a97191f78e60a2ac1c3dd28f5d61bc
Sebastian- <hmurgu@interchange.ubc.ca>
2019-02-11 11:39:58 -0800

    feat: Added logout functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import {
@@ -9,60 +9,124 @@ import {
   Menu,
   MenuItem,
   Avatar,
+  withStyles
 } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
-import AccountIcon from '@material-ui/icons/AccountCircle'
+import ArrowIcon from '@material-ui/icons/ArrowDropDown'
+import { setAuthUser } from '../actions/authedUser';
 
 class NavBar extends Component {
   state = {
-    anchorEl: null
+    navAnchor: null,
+    logoutAnchor: null
   }
 
-  handleMenu = event => {
-    this.setState({ anchorEl: event.currentTarget })
+  handleNavMenu = event => {
+    this.setState({ navAnchor: event.currentTarget })
+  }
+
+  handleLogoutMenu = (event) => {
+    this.setState({ logoutAnchor: event.currentTarget })
   }
   
-  handleClose = () => {
-    this.setState({ anchorEl: null });
+  handleNavClose = () => {
+    this.setState({ navAnchor: null });
+  }
+
+  handleLogoutClose = () => {
+    this.setState({ logoutAnchor: null });
+  }
+
+  handleLogout = () => {
+    this.setState({ logoutAnchor: null })
+    this.props.dispatch(setAuthUser(null))
+    this.props.history.push('/')
   }
 
   render() {
-    const { userName, avatar } = this.props
-    const { anchorEl } = this.state
-    const open = Boolean(anchorEl)
+    const { userName, avatar, classes } = this.props
+    const { navAnchor, logoutAnchor } = this.state
+    const navOpen = Boolean(navAnchor)
+    const logoutOpen = Boolean(logoutAnchor)
 
     return (
       <AppBar>
         <Toolbar>
-          <div>
-            <IconButton onClick={this.handleMenu}>
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={open}
-              onClose={this.handleClose}
-            >
-              <Link to='/'><MenuItem onClick={this.handleClose}>Home</MenuItem></Link>
-              <Link to='/add'><MenuItem onClick={this.handleClose}>New Question</MenuItem></Link>
-              <Link to='/leaderboard'><MenuItem onClick={this.handleClose}>Leaderboard</MenuItem></Link>
-            </Menu>
+          <IconButton className={classes.menuButton} onClick={this.handleNavMenu}>
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            anchorEl={navAnchor}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={navOpen}
+            onClose={this.handleNavClose}
+          >
+            <Link to='/'><MenuItem onClick={this.handleNavClose}>Home</MenuItem></Link>
+            <Link to='/add'><MenuItem onClick={this.handleNavClose}>New Question</MenuItem></Link>
+            <Link to='/leaderboard'><MenuItem onClick={this.handleNavClose}>Leaderboard</MenuItem></Link>
+          </Menu>
+          <div className={classes.grow} />
+          <div className={classes.userInfo}>
+            <Typography variant='h5'>{userName}</Typography>
+            {avatar
+              ? <Avatar className={classes.avatar} src={avatar} />
+              : null
+            }
+            {userName
+              ? <Fragment>
+                  <IconButton onClick={this.handleLogoutMenu}>
+                    <ArrowIcon />
+                  </IconButton>
+                  <Menu
+                    anchorEl={logoutAnchor}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    open={logoutOpen}
+                    onClose={this.handleLogoutClose}
+                  >
+                    <MenuItem onClick={this.handleLogout}>Sign Out</MenuItem>
+                  </Menu>
+                </Fragment>
+              : null
+            }
           </div>
-          {avatar ? <Avatar src={avatar} /> : <AccountIcon />}
-          <Typography variant='h5'>{userName}</Typography>
         </Toolbar>
       </AppBar>
     )
   }
 }
+
+const styles = theme => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  userInfo: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  avatar: {
+    marginLeft: 10,
+    marginRight: 10
+  }
+})
 
 function mapStateToProps ({ users, authedUser }) {
   const user = users[authedUser]
@@ -75,4 +139,4 @@ function mapStateToProps ({ users, authedUser }) {
     : {}
 }
 
-export default connect(mapStateToProps)(NavBar)
+export default withStyles(styles)(connect(mapStateToProps)(NavBar))
