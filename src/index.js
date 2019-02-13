7e06c566f281b9f1c1c06c55b979410a706b05a1
Sebastian- <hmurgu@interchange.ubc.ca>
2019-02-13 11:57:27 -0800

    chore: configuration for build

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import reducer from './reducers'
-import middleware from './middleware'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import middleware from './middleware';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const store = createStore(reducer, middleware)
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
-  <Router>
+  <Router basename='/would-you-rather'>
     <Provider store={store}>
       <App />
     </Provider>
   </Router>,
-document.getElementById('root'))
+  document.getElementById('root'));
