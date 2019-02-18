321b2547e45978491dd530e6e039320dac3b7afb
Sebastian- <hmurgu@interchange.ubc.ca>
2019-02-18 09:09:32 -0800

    refactor: NavBar menus delegated to separate components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,118 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import {
   AppBar,
   Toolbar,
-  IconButton,
   Typography,
-  Menu,
-  MenuItem,
   Avatar,
-  withStyles
+  withStyles,
 } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import ArrowIcon from '@material-ui/icons/ArrowDropDown';
-import { setAuthUser } from '../actions/authedUser';
+import { unstable_useMediaQuery as useMediaQuery } from '@material-ui/core/useMediaQuery';
+import NavTabs from './NavTabs';
+import NavMenu from './NavMenu';
+import LogoutMenu from './LogoutMenu';
 
-class NavBar extends Component {
-  state = {
-    navAnchor: null,
-    logoutAnchor: null
-  }
+const navOptions = [
+  {path: '/', name: 'Home'},
+  {path: '/add', name: 'New Question'},
+  {path: '/leaderboard', name: 'Leaderboard'},
+];
 
-  handleNavMenu = event => {
-    this.setState({ navAnchor: event.currentTarget });
-  }
+const NavBar = (props) => {
+  const { userName, avatar, classes } = props;
+  const showNavTabs = useMediaQuery('(min-width:800px)');
 
-  handleLogoutMenu = (event) => {
-    this.setState({ logoutAnchor: event.currentTarget });
-  }
-  
-  handleNavClose = () => {
-    this.setState({ navAnchor: null });
-  }
-
-  handleLogoutClose = () => {
-    this.setState({ logoutAnchor: null });
-  }
-
-  handleLogout = () => {
-    this.setState({ logoutAnchor: null });
-    this.props.setAuthUser(null);
-    this.props.history.push('/');
-  }
-
-  render() {
-    const { userName, avatar, classes } = this.props;
-    const { navAnchor, logoutAnchor } = this.state;
-    const navOpen = Boolean(navAnchor);
-    const logoutOpen = Boolean(logoutAnchor);
-
-    return (
-      <AppBar>
-        <Toolbar>
-          <IconButton
-            disabled={!userName}
-            className={classes.menuButton}
-            onClick={this.handleNavMenu}>
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            anchorEl={navAnchor}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
-            }}
-            open={navOpen}
-            onClose={this.handleNavClose}
-          >
-            <Link to='/'><MenuItem onClick={this.handleNavClose}>Home</MenuItem></Link>
-            <Link to='/add'><MenuItem onClick={this.handleNavClose}>New Question</MenuItem></Link>
-            <Link to='/leaderboard'><MenuItem onClick={this.handleNavClose}>Leaderboard</MenuItem></Link>
-          </Menu>
-          <div className={classes.grow} />
-          {userName ? 
-            <div className={classes.userInfo}>
-              <Typography variant='h5'>{userName}</Typography>
-              <Avatar className={classes.avatar} src={avatar} />
-              <IconButton onClick={this.handleLogoutMenu}>
-                <ArrowIcon />
-              </IconButton>
-              <Menu
-                anchorEl={logoutAnchor}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={logoutOpen}
-                onClose={this.handleLogoutClose}
-              >
-                <MenuItem onClick={this.handleLogout}>Sign Out</MenuItem>
-              </Menu>
-            </div>
-            : null}
-        </Toolbar>
-      </AppBar>
-    );
-  }
-}
+  return (
+    <AppBar>
+      <Toolbar>
+        {showNavTabs 
+          ? <NavTabs navOptions={navOptions} />
+          : <NavMenu disabled={!userName} navOptions={navOptions} />
+        }
+        <div className={classes.grow} />
+        {userName ? 
+          <div className={classes.userInfo}>
+            <Typography variant='h5'>{userName}</Typography>
+            <Avatar className={classes.avatar} src={avatar} />
+            <LogoutMenu />
+          </div>
+          : null}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 const styles = {
   grow: {
     flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
   },
   userInfo: {
     display: 'flex',
@@ -125,14 +57,6 @@ const styles = {
   }
 };
 
-function mapDispatchToProps (dispatch) {
-  return ({
-    setAuthUser: (userID) => {
-      dispatch(setAuthUser(userID));
-    }
-  });
-}
-
 function mapStateToProps ({ users, authedUser }) {
   const user = users[authedUser];
 
@@ -144,4 +68,4 @@ function mapStateToProps ({ users, authedUser }) {
     : {};
 }
 
-export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(NavBar));
+export default withStyles(styles)(connect(mapStateToProps)(NavBar));
