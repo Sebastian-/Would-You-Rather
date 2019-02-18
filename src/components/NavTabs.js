327060f49362ec1f8283e1cf5f86d38849c91e01
Sebastian- <hmurgu@interchange.ubc.ca>
2019-02-18 08:23:51 -0800

    feat: created NavTabs component

---
+++ src/components/NavTabs.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Tabs,
+  Tab
+} from '@material-ui/core';
+
+class NavTabs extends Component {
+
+  state = {
+    value: 0
+  }
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { value } = this.state;
+    const { navOptions } = this.props;
+
+    return (
+      <Tabs value={value} onChange={this.handleChange}>
+        {navOptions.map(({ path, name }) => (
+          <Tab key={path} component={Link} to={path} label={name} />
+        ))}
+      </Tabs>
+    );
+  }
+}
+
+export default NavTabs;
